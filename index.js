2e557714c4db794a12cb8f982f3ceeffd96d39d3
SouravBiswas238 <souravbiswas427@gmail.com>
2022-05-26 14:56:23 +0600

    successfully add admin api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -13,8 +14,36 @@ app.use(express.json());
 
 
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xv9wx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+const verifyAdmin = async (req, res, next) => {
+    const requester = req.decoded.email;
+    const requesterAccount = await userCollection.findOne({ email: requester });
+    if (requesterAccount.role === 'admin') {
+        next();
+    }
+    else {
+        res.status(403).send({ message: 'forbidden' });
+    }
+}
 
 async function run() {
     try {
@@ -23,18 +52,20 @@ async function run() {
         const productCollection = client.db("flash-electronic").collection("product");
         const orderCollection = client.db("flash-electronic").collection("order");
         const userCollection = client.db("flash-electronic").collection("user");
+        const reviewCollection = client.db("flash-electronic").collection("review");
 
         // post user information
         app.post('/user', async (req, res) => {
             const newUser = req.body;
             const query = { email: newUser.email }
 
+            const token = jwt.sign({ email: newUser.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             const exists = await orderCollection.findOne(query);
             if (exists) {
-                return res.send({ success: false, user: exists })
+                return res.send({ success: false, user: exists, token })
             }
             const result = await userCollection.insertOne(newUser);
-            res.send({ success: true, result });
+            res.send({ success: true, result, token });
         });
 
         // get all users
@@ -52,8 +83,14 @@ async function run() {
             const user = await userCollection.findOne(query);
             res.send(user);
         })
-
-        // get single email information
+        // get admin email
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+        // put single email information
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             console.log(email);
@@ -66,6 +103,17 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         })
+        // Put user admin email
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
         // get all product
         app.get('/product', async (req, res) => {
             const query = {};
@@ -73,7 +121,8 @@ async function run() {
             products = await cursor.toArray();
             res.send(products);
         })
-        // get single id data
+
+        // get single id product
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -82,7 +131,7 @@ async function run() {
         })
 
         // post ordering data
-        app.post('/order', async (req, res) => {
+        app.post('/order', verifyJWT, async (req, res) => {
             const order = req.body;
             const query = { orderName: order.orderName, minOrder: order.minOrder }
             const exists = await orderCollection.findOne(query);
@@ -92,6 +141,37 @@ async function run() {
             const result = await orderCollection.insertOne(order);
             res.send({ success: true, result });
         })
+        // get single email order
+        app.get('/order/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const result = await orderCollection.find(query).toArray();
+                return res.send(result);
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
+
+        })
+
+
+        // post review data
+        app.post('/review', async (req, res) => {
+            const newReview = req.body;
+            const result = await reviewCollection.insertOne(newReview);
+            res.send(result);
+        });
+        // get review data
+        app.get('/review', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            reviews = await cursor.toArray();
+            res.send(reviews);
+        });
 
     }
     finally {
